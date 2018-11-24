3261025de1ec7ac3022787d160e5ff4034ceb375
cokora <cokora525@gmail.com>
2018-11-24 15:22:40 +0800

    update listing details

--- admin/src/pages/Listing.vue
+++ admin/src/pages/Listing.vue
@@ -11,142 +11,6 @@
   <!-- CARD -->
       <willow-layout-section primary>
 
-       <b-card class="mb-2" >
-        <h6 class="heading">Unit Details</h6>
-        <b-row class="mb-2">
-          <b-col >
-            <label for="inputLive">Unit</label>
-            <b-form-input type="text" v-model="listing.unit">
-            </b-form-input>
-          </b-col>
-
-        </b-row>
-
-        <b-row class="mb-2">
-          <b-col :cols="6">
-            <label for="inputLive">Beds</label>
-            <b-form-input type="text" v-model="listing.beds">
-            </b-form-input>
-          </b-col>
-
-          <b-col :cols="6">
-            <label for="inputLive">Baths</label>
-            <b-form-input type="text"  v-model="listing.baths">
-            </b-form-input>
-          </b-col>
-
-          <b-col :cols="6">
-            <label for="inputLive">SqFt</label>
-            <b-form-input type="text" v-model="listing.sqft">
-            </b-form-input>
-          </b-col>
-
-        </b-row>
-
-        <b-row class="mb-2">
-          <b-col>
-            <label for="inputLive">Description</label>
-            <b-form-textarea id="textarea1"
-                            v-model="text"
-                            :rows="3"
-                            :max-rows="6">
-            </b-form-textarea>
-          </b-col>
-
-        </b-row>
-
-       </b-card>
-
-      <b-card class="mb-2" >
-        <h6 class="heading">Features</h6>
-        <b-row class="mb-2">
-          <b-col :cols="8">
-          <b-form-group>
-            <b-form-checkbox id="checkbox1" v-model="status" value="accepted" unchecked-value="not_accepted">Business Center</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox2" v-model="status" value="accepted" unchecked-value="not_accepted">Fitness Center</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox3" v-model="status" value="accepted" unchecked-value="not_accepted">Residents" Lounge</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox4" v-model="status" value="accepted" unchecked-value="not_accepted">Wheelchair Access</b-form-checkbox>
-          </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox16" v-model="status" value="accepted" unchecked-value="not_accepted">Swimming Pool</b-form-checkbox>
-            </b-form-group>
-        </b-col>
-        <b-col :cols="8">
-          <b-form-group>
-            <b-form-checkbox id="checkbox5" v-model="status" value="accepted" unchecked-value="not_accepted">Garage Parking</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox6" v-model="status" value="accepted" unchecked-value="not_accepted">Roof Deck</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox7" v-model="status" value="accepted" unchecked-value="not_accepted">Door Person</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox8" v-model="status" value="accepted" unchecked-value="not_accepted">Onsite Management</b-form-checkbox>
-          </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox13" v-model="status" value="accepted" unchecked-value="not_accepted">Elevator</b-form-checkbox>
-            </b-form-group>
-        </b-col>
-        <b-col :cols="8">
-          <b-form-group>
-            <b-form-checkbox id="checkbox9" v-model="status" value="accepted" unchecked-value="not_accepted">Dry Cleaning</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox10" v-model="status" value="accepted" unchecked-value="not_accepted">Outdoor Space</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox11" v-model="status" value="accepted" unchecked-value="not_accepted">Storage</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox12" v-model="status" value="accepted" unchecked-value="not_accepted">Package Service</b-form-checkbox>
-          </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox14" v-model="status" value="accepted" unchecked-value="not_accepted">Laundry</b-form-checkbox>
-            </b-form-group>
-          </b-col>
-
-        </b-row>
-
-       </b-card>
-
-      <b-card class="mb-2" >
-        <h6 class="heading"> Building Images</h6>
-        <b-row class="mb-2">
-          <b-col :cols="24">
-            <div class="float-right p-1">
-              <willow-file-input-multiple @uploadedFiles="getBuildingImages" :url="`/propertys/${this.propertyId}/upload`" identifier="image">
-                  Add images
-                </willow-file-input-multiple>
-            </div>
-          </b-col>
-          <b-col :cols="24">
-              <willow-product-images
-                :images="buildingImages"
-              @remove-img="deleteBuildingImage($event)"
-              >
-              </willow-product-images>
-          </b-col>
-        </b-row>
-       </b-card>
-        <b-card class="mb-2" >
-        <h6 class="heading"> Units Images</h6>
-        <b-row class="mb-2">
-          <b-col :cols="24">
-            <div class="float-right p-1">
-              <willow-file-input-multiple @uploadedFiles="getUnitImages" :url="`/units/${this.unitId}/upload`" identifier="image">
-                  Add images
-                </willow-file-input-multiple>
-            </div>
-          </b-col>
-          <b-col :cols="24">
-              <willow-product-images
-                :images="unitImages"
-              @remove-img="deleteUnitImage($event)"
-              >
-              </willow-product-images>
-          </b-col>
-        </b-row>
-       </b-card>
-
-    </willow-layout-section>
-
-    <willow-layout-section secondary>
-
        <b-card class="mb-2" style="background-color: #f8f9fa ;">
         <h6 class="heading">Listing Settings</h6>
         <b-row>
@@ -170,13 +34,13 @@
         <b-row>
           <b-col :cols="6">
             <label for="inputLive">Rent</label>
-            <b-form-input type="text" v-model="listing.rent">
+            <b-form-input type="text" disabled v-model="listing.rent">
             </b-form-input>
           </b-col>
 
           <b-col :cols="6">
             <label for="inputLive">Deposit</label>
-            <b-form-input type="text" v-model="listing.deposit">
+            <b-form-input type="text" disabled v-model="listing.deposit">
             </b-form-input>
           </b-col>
 
@@ -185,7 +49,7 @@
         <b-row>
           <b-col :cols="6">
             <label for="inputLive">Lease Terms</label>
-            <b-form-input type="text" v-model="listing.lease_terms">
+            <b-form-input type="text" disabled v-model="listing.lease_terms">
             </b-form-input>
 
           </b-col>
@@ -198,6 +62,7 @@
               type="date"
               placeholder="First"
               v-model="listing.date_available"
+              disabled
             >
             </b-form-input>
           </b-col>
@@ -206,15 +71,52 @@
         <b-row class="mt-4">
           <b-col>
             <b-form-group label="Section 8">
-              <b-form-radio-group id="radios2" v-model="selected" name="radioSubComponent">
+              <b-form-radio-group id="radios2" v-model="selected" disabled name="radioSubComponent">
                 <b-form-radio value="first">Yes</b-form-radio>
                 <b-form-radio value="second">No</b-form-radio>
               </b-form-radio-group>
             </b-form-group>
           </b-col>
         </b-row>
-
       </b-card>
+       <b-card>
+        <h6 class="heading"> Building Images</h6>
+        <b-row class="mb-2">
+          <b-col :cols="24">
+            <div class="float-right p-1">
+              <willow-file-input-multiple @uploadedFiles="getBuildingImages" :url="`/propertys/${this.propertyId}/upload`" identifier="image">
+                  Add images
+                </willow-file-input-multiple>
+            </div>
+          </b-col>
+          <b-col :cols="24">
+              <willow-product-images
+                :images="buildingImages"
+              @remove-img="deleteBuildingImage($event)"
+              >
+              </willow-product-images>
+          </b-col>
+        </b-row>
+        </b-card>
+        <b-card class="mb-2" >
+        <h6 class="heading"> Units Images</h6>
+        <b-row class="mb-2">
+          <b-col :cols="24">
+            <div class="float-right p-1">
+              <willow-file-input-multiple @uploadedFiles="getUnitImages" :url="`/units/${this.unitId}/upload`" identifier="image">
+                  Add images
+                </willow-file-input-multiple>
+            </div>
+          </b-col>
+          <b-col :cols="24">
+              <willow-product-images
+                :images="unitImages"
+              @remove-img="deleteUnitImage($event)"
+              >
+              </willow-product-images>
+          </b-col>
+        </b-row>
+       </b-card>
 
     </willow-layout-section>
 
--- client/src/pages/listingDetails.vue
+++ client/src/pages/listingDetails.vue
@@ -3,102 +3,7 @@
   <willow-layout >
   <!-- CARD -->
       <willow-layout-section primary>
-       <b-card class="mb-2" >
-        <h6 class="heading">Unit Details</h6>
-        <b-row class="mb-2">
-          <b-col >
-            <label for="inputLive">Unit</label>
-            <b-form-input type="text" v-model="listing.unit">
-            </b-form-input>
-          </b-col>
-        </b-row>
-        <b-row class="mb-2">
-          <b-col :cols="6">
-            <label for="inputLive">Beds</label>
-            <b-form-input type="text" v-model="listing.beds">
-            </b-form-input>
-          </b-col>
-
-          <b-col :cols="6">
-            <label for="inputLive">Baths</label>
-            <b-form-input type="text"  v-model="listing.baths">
-            </b-form-input>
-          </b-col>
-
-          <b-col :cols="6">
-            <label for="inputLive">SqFt</label>
-            <b-form-input type="text" v-model="listing.sqft">
-            </b-form-input>
-          </b-col>
-
-        </b-row>
-
-        <b-row class="mb-2">
-          <b-col>
-            <label for="inputLive">Description</label>
-            <b-form-textarea id="textarea1"
-                            v-model="text"
-                            :rows="3"
-                            :max-rows="6">
-            </b-form-textarea>
-          </b-col>
-
-        </b-row>
-
-       </b-card>
-
-      <b-card class="mb-2" >
-        <h6 class="heading">Features</h6>
-        <b-row class="mb-2">
-          <b-col :cols="8">
-          <b-form-group>
-            <b-form-checkbox id="checkbox1" v-model="status" value="accepted" unchecked-value="not_accepted">Business Center</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox2" v-model="status" value="accepted" unchecked-value="not_accepted">Fitness Center</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox3" v-model="status" value="accepted" unchecked-value="not_accepted">Residents" Lounge</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox4" v-model="status" value="accepted" unchecked-value="not_accepted">Wheelchair Access</b-form-checkbox>
-          </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox16" v-model="status" value="accepted" unchecked-value="not_accepted">Swimming Pool</b-form-checkbox>
-            </b-form-group>
-        </b-col>
-        <b-col :cols="8">
-          <b-form-group>
-            <b-form-checkbox id="checkbox5" v-model="status" value="accepted" unchecked-value="not_accepted">Garage Parking</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox6" v-model="status" value="accepted" unchecked-value="not_accepted">Roof Deck</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox7" v-model="status" value="accepted" unchecked-value="not_accepted">Door Person</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox8" v-model="status" value="accepted" unchecked-value="not_accepted">Onsite Management</b-form-checkbox>
-          </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox13" v-model="status" value="accepted" unchecked-value="not_accepted">Elevator</b-form-checkbox>
-            </b-form-group>
-        </b-col>
-        <b-col :cols="8">
-          <b-form-group>
-            <b-form-checkbox id="checkbox9" v-model="status" value="accepted" unchecked-value="not_accepted">Dry Cleaning</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox10" v-model="status" value="accepted" unchecked-value="not_accepted">Outdoor Space</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox11" v-model="status" value="accepted" unchecked-value="not_accepted">Storage</b-form-checkbox>
-            </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox12" v-model="status" value="accepted" unchecked-value="not_accepted">Package Service</b-form-checkbox>
-          </b-form-group><b-form-group>
-            <b-form-checkbox id="checkbox14" v-model="status" value="accepted" unchecked-value="not_accepted">Laundry</b-form-checkbox>
-            </b-form-group>
-          </b-col>
-
-        </b-row>
-
-       </b-card>
-
-    </willow-layout-section>
-
-    <willow-layout-section primary>
-
-       <b-card class="mb-1" style="background-color: #f8f9fa ;">
+      <b-card class="mb-2" style="background-color: #f8f9fa ;">
         <h6 class="heading">Listing Settings</h6>
         <b-row>
           <b-col :cols="6">
@@ -121,13 +26,13 @@
         <b-row>
           <b-col :cols="6">
             <label for="inputLive">Rent</label>
-            <b-form-input type="text" v-model="listing.rent">
+            <b-form-input type="text" disabled v-model="listing.rent">
             </b-form-input>
           </b-col>
 
           <b-col :cols="6">
             <label for="inputLive">Deposit</label>
-            <b-form-input type="text" v-model="listing.deposit">
+            <b-form-input type="text" disabled v-model="listing.deposit">
             </b-form-input>
           </b-col>
 
@@ -136,7 +41,7 @@
         <b-row>
           <b-col :cols="6">
             <label for="inputLive">Lease Terms</label>
-            <b-form-input type="text" v-model="listing.lease_terms">
+            <b-form-input type="text" disabled v-model="listing.lease_terms">
             </b-form-input>
 
           </b-col>
@@ -149,6 +54,7 @@
               type="date"
               placeholder="First"
               v-model="listing.date_available"
+              disabled
             >
             </b-form-input>
           </b-col>
@@ -157,14 +63,13 @@
         <b-row class="mt-4">
           <b-col>
             <b-form-group label="Section 8">
-              <b-form-radio-group id="radios2" v-model="selected" name="radioSubComponent">
+              <b-form-radio-group id="radios2" v-model="selected" disabled name="radioSubComponent">
                 <b-form-radio value="first">Yes</b-form-radio>
                 <b-form-radio value="second">No</b-form-radio>
               </b-form-radio-group>
             </b-form-group>
           </b-col>
         </b-row>
-
       </b-card>
       <b-row>
         <b-card class="mb-2" >
