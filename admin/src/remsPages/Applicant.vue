6eb786c5d70968cf0e7e7e72b83dfbd9114c410d
Dylan <dylanhallagan@msn.com>
2018-11-25 23:33:33 -0500

    Added port to .env (#166)

--- admin/src/remsPages/Applicant.vue
+++ admin/src/remsPages/Applicant.vue
@@ -106,7 +106,7 @@ export default {
         breadcrumbs: [
           {
             text: 'Applicants',
-            href: '/Applicants'
+            href: '/Admin/Applicants'
           }
         ]
       },
--- server/src/core/database.ts
+++ server/src/core/database.ts
@@ -24,7 +24,7 @@ export class Database {
     return createConnection({
         type: "mysql",
         host: process.env.DB_HOST,
-        port: 8889,
+        port: parseInt(process.env.DB_PORT || '3306'),
         username: process.env.DB_USER,
         password: process.env.DB_PASS,
         database: process.env.DB_DATABASE,
